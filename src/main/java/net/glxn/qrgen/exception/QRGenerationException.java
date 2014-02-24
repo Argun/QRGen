37373202f04c9d8a19cde4b217d21da2c19de6f1
sunjz <sunjz@nextop.cn>
2014-02-24 22:10:07 +0900

    serial

--- src/main/java/net/glxn/qrgen/exception/QRGenerationException.java
+++ src/main/java/net/glxn/qrgen/exception/QRGenerationException.java
@@ -1,5 +1,6 @@
 package net.glxn.qrgen.exception;
 
+@SuppressWarnings("serial")
 public class QRGenerationException extends RuntimeException {
     public QRGenerationException(String message, Throwable underlyingException) {
         super(message, underlyingException);
